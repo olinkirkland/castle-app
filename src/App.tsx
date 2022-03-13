6dd0c9fd8ce894f182ff572f7d1e4894e4764b83
Olin Kirkland <me@olinkirk.land>
2022-03-14 00:03:27 +0100

    Implement React front-end struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './assets/css/styles.css';
+import CastleTile from './components/CastleTile';
 
 function App() {
+  const [castles, setCastles] = useState({});
+
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/data.json`)
+      .then((response) => response.json())
+      .then((data) => setCastles(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <ul className="castle-list">
+        {Object.keys(castles).map((key) => (
+          <li>
+            <CastleTile castle={castles[key as keyof typeof castles]} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
